561871282ddea06d0a14f674f742ae3c66b9a0ef
Yash1118jain <yashjain11dec@gmail.com>
2026-06-24 00:28:01 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -3,8 +3,8 @@ from src.logger import logging
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
     error_message = "Error occurred in script: [{0}] at line number: [{1}] error message: [{2}]".format(file_name, line_number, str(error))
-    file_name,exc_tb.tb_lineno, str(error)
     
     return error_message
 
--- src/logger.py
+++ src/logger.py
@@ -8,7 +8,7 @@ os.makedirs(logpath, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logpath, LOG_FILE)
 
-log.basicConfig(
+logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asctime)s] %(lineno)d %(name)s %(levelname)s - %(message)s",
     level=logging.INFO
